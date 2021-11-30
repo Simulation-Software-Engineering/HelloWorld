bb4e0898ccdda51cd2e3cdd1897ae0defdc6dc9a
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-11-30 16:38:20 +0100

    Step 3

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -8,4 +8,8 @@ set(CPACK_PACKAGE_CONTACT "firstname.lastname@example.com")
 set(CPACK_PACKAGE_MAINTAINERS "SSE lecturers ${CPACK_PACKAGE_CONTACT}")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://simulation-software-engineering.github.io/homepage/")
 
+# Debian specific changes
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES)
+
 include(CPack)
